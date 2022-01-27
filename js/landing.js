d7b9d37bcf7ef13e4d996394f17309850e7bb5de
israaHamdi <israaaburayya2012@gmail.com>
2022-01-27 12:20:25 +0200

    add task to page and local storage RELATES#7

---
+++ js/landing.js
@@ -0,0 +1,12 @@
+let inputName=document.getElementById('name')
+let nextPage=document.getElementById('start')
+let localName=document.getElementById('local-name')
+let yourName
+
+//go to the home page
+nextPage.addEventListener('click',()=>{
+    yourName=inputName.value
+    if (yourName == '') return
+    localStorage.setItem('name',yourName)
+    location.href = "/html/homePage.html";
+})
